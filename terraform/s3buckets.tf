9ab6446ce4f89a5edf3557adeab8c11d7b78ccc6
ehrnjic <ehrnjic@gmail.com>
2021-06-14 10:49:10 +0200

    append code to create cloud front distribution

--- terraform/s3buckets.tf
+++ terraform/s3buckets.tf
@@ -1,4 +1,3 @@
-
 terraform {
   required_providers {
     aws = {
@@ -13,7 +12,7 @@ provider "aws" {
 }
 
 /* 
-   it is possible to use the type list if we need to create
+   it is possible to use the list if we need to create
    a larger number of s3buckets without repeating the code
 */
 
@@ -48,4 +47,45 @@ resource "aws_s3_bucket_public_access_block" "acl" {
   block_public_policy = true
   restrict_public_buckets = true
   ignore_public_acls = true
+}
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.production-bucket.bucket_regional_domain_name
+    origin_id   = aws_s3_bucket.production-bucket.id
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = aws_s3_bucket.production-bucket.id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+    restrictions {
+        geo_restriction {
+          restriction_type = "whitelist"
+          locations        = ["BA", "RS", "HR", "US", "CA", "GB", "DE"]
+        }
+      }
+
+    viewer_certificate {
+      cloudfront_default_certificate = true
+    }
 }
